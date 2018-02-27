3fead17cdd871c84747a71b43131c9bdfaa75b0c
MohammedAtefHassan <mahbfci1361998@gmail.com>
2018-02-28 00:17:28 +0200

    first commit

---
+++ UVA/UVA-11661.cpp
@@ -0,0 +1,67 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+using namespace std;
+#define  ll long long
+#define  ull unsigned long long
+#define  ul unsigned long
+#define all(v) v.begin(),v.end()
+#define sz(n) (int)n.size()
+#define pb(s) push_back(s);
+#define format(n) fixed<<setprecision(n)
+#define finl "\n"
+#define Mohammed_Atef_Hassan fastt();
+void fast()
+{
+	std::ios_base::sync_with_stdio(0);
+	cin.tie(NULL); cout.tie(NULL);
+}
+void infile()
+{
+	freopen("input.in", "r", stdin);
+}
+void outfile()
+{
+	freopen("output.in", "w", stdout);
+}
+
+void fastt(){
+	fast();
+	//	infile();
+	//	outfile();
+}
+int main()
+{   
+	Mohammed_Atef_Hassan
+	while (true)
+	{
+		ll n;
+		ll mn = (ll)1e18;
+		cin >> n;
+		if (n == 0)
+			return 0;
+		string s;
+		cin >> s;
+		ll idx = -1;
+		for (ll i = 0; i < s.size(); i++)
+		{
+			if (s[i] != '.')
+			{
+				if (idx == -1 && s[i] != 'Z')
+					idx = i;
+				else if (s[i] == 'Z')
+					mn = 0;
+				else
+				{
+					if (s[i] == s[idx])
+						idx = i;
+					else if (s[i] != s[idx])
+					{
+						mn = min(mn, i - idx);
+						idx = i;
+					}
+				}
+			}
+		}
+		cout << mn << finl;
+	}
+}
