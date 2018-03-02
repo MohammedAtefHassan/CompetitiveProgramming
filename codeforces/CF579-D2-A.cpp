f312ea731654b27d9f2ae4a233771fbaf3a43164
MohammedAtefHassan <mahbfci1361998@gmail.com>
2018-03-02 16:04:18 +0200

    first commit

---
+++ codeforces/CF579-D2-A.cpp
@@ -0,0 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+using namespace std;
+#define  ll long long
+#define  ull unsigned long long
+#define  ul unsigned long
+#define all(v) v.begin(),v.end()
+#define sz(n) (int)n.size()
+#define pb(s) push_back(s);
+#define format(n) fixed<<setprecision(n)
+#define finl "\n"
+#define Mohammed_Atef_Hassan fastt();
+void fast()
+{
+	std::ios_base::sync_with_stdio(0);
+	cin.tie(NULL); cout.tie(NULL);
+}
+void infile()
+{
+	freopen("input.in", "r", stdin);
+}
+void outfile()
+{
+	freopen("output.in", "w", stdout);
+}
+
+void fastt(){
+	fast();
+	//	infile();
+	//	outfile();
+}
+ll arr[26];
+int main()
+{
+	Mohammed_Atef_Hassan
+		ll n;
+	cin >> n;
+	int cnt = 0;
+	while (n)
+	{
+		cnt += n % 2;
+		n /= 2;
+	}
+	cout << cnt << finl;
+}
