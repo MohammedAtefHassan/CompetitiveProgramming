65ce505f5755cd6d8a4c2ab2d6a7374930262636
MohammedAtefHassan <mahbfci1361998@gmail.com>
2018-03-03 17:56:31 +0200

    first commit

---
+++ UVA/UVA-483.cpp
@@ -0,0 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+using namespace std;
+#define  ll long long
+#define  ull unsigned long long
+#define  ul unsigned long
+#define all(v) v.begin(),v.end()
+#define sz(n) (int)n.size()
+#define pb(s) push_back(s);
+#define format(n) fixed<<setprecision(n)
+#define finl "\n"
+#define Mohammed_Atef_Hassan fastt();
+void fast()
+{
+	std::ios_base::sync_with_stdio(0);
+	cin.tie(NULL); cout.tie(NULL);
+}
+void infile()
+{
+	freopen("input.in", "r", stdin);
+}
+void outfile()
+{
+	freopen("output.in", "w", stdout);
+}
+
+void fastt(){
+	fast();
+//	infile();
+	//	outfile();
+}
+int main()
+{
+	Mohammed_Atef_Hassan
+
+		string s = "",t;
+	while (getline(cin, s))
+	{
+		t = "";
+		for (int i = 0; i < s.size(); i++)
+		{
+			if (s[i] != ' ')
+				t =s[i]+t;
+			else
+			{
+				cout << t << " ";
+				t = "";
+			}
+		}
+		if (t !="")
+			cout << t;
+		cout << finl;
+	}
+		
+}
