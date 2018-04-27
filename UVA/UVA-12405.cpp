cb7345b8a0c9ff5ef239c1e5749b3ec09a8ff84d
Mohammed_Atef_Hassan <36866773+MohammedAtefHassan@users.noreply.github.com>
2018-04-27 20:44:01 +0200

    first commit

--- UVA/UVA-12405.cpp
+++ UVA/UVA-12405.cpp
@@ -5,47 +5,44 @@ using namespace std;
 #define  ull unsigned long long
 #define  ul unsigned long
 #define all(v) v.begin(),v.end()
-#define sz(n) (int)n.size()
-#define pb(s) push_back(s);
 #define format(n) fixed<<setprecision(n)
 #define finl "\n"
-#define Mohammed_Atef_Hassan fastt();
+#define Mohammed_Atef_Hassan fast();
 #define mod 1000000007
-#define INF 1000000000000000000;
-void fast()
-{
+#define INF 1000000
+#define PI acos(-1)
+void fast(){
 	std::ios_base::sync_with_stdio(0);
 	cin.tie(NULL); cout.tie(NULL);
-}
-void fastt(){
-	fast();
-#ifndef ONLINE_JUDGE
-	freopen("input.in", "r", stdin);
 
-	freopen("output.in", "w", stdout);
+#ifndef ONLINE_JUDGE
+	//freopen("input.in", "r", stdin);
+	// freopen("output.out", "w", stdout);
 #endif
 }
 int main()
 {
 	Mohammed_Atef_Hassan
-		int t, n; string s;
-	int cnt;
+		int t;
+	int n; string s;
 	while (cin >> t)
 	{
-		for (int j = 1; j <= t; j++)
+		for (int k = 1; k <= t;k++)
 		{
-			cnt = 0;
 			cin >> n >> s;
-			int i = 0;
-			for (int i = 0; i < s.size();)
+			int mn = 0;
+			for (int i = 0; i < n;)
 			{
 				if (s[i] == '.')
-					cnt++, i += 3;
+				{
+					mn++;
+					i += 3;
+				}
 				else
 					i++;
 			}
-			cout << "Case " << j << ": " << cnt << finl;
+			cout <<"Case "<<k<<": "<<mn << finl;
 		}
-		
 	}
-}
+
+} 
