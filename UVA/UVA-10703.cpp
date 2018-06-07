0e14ddcfb617a77954f95cdc02815a4224256c78
Mohammed_Atef_Hassan <36866773+MohammedAtefHassan@users.noreply.github.com>
2018-06-07 14:05:41 +0200

    first commit

--- UVA/UVA-10703.cpp
+++ UVA/UVA-10703.cpp
@@ -9,58 +9,68 @@ using namespace std;
 #define pb(s) push_back(s);
 #define format(n) fixed<<setprecision(n)
 #define finl "\n"
-#define Mohammed_Atef_Hassan fastt();
+#define Mohammed_Atef_Hassan fast();
 #define mod 1000000007
-void fast()
-{
+#define INF 1000000000000000000;
+#define PI acos(-1)
+void fast(){
+
 	std::ios_base::sync_with_stdio(0);
 	cin.tie(NULL); cout.tie(NULL);
-}
-void infile()
-{
-	freopen("input.in", "r", stdin);
-}
-void outfile()
-{
-	freopen("output.in", "w", stdout);
-}
 
-void fastt(){
-	fast();
-	//    infile();
-	//	outfile();
+#ifdef ONLINE_JUDGE
+//	freopen("palindrome.in", "r", stdin);
+
+	//freopen("palindrome.out", "w", stdout);
+#endif
+#ifndef ONLINE_JUDGE
+//	freopen("input.in", "r", stdin);
+
+	//freopen("output.out", "w", stdout);
+#endif
 }
-bool arr[502][502];
+int arr[502][502];
+int cnt[502][502];
 int main()
 {
 	Mohammed_Atef_Hassan
-		int w, h, n;
-	int x1, y1, x2, y2;
-	while (cin >> w >> h >> n&&w&&h)
+		int n, m,k;
+	while (cin >> n >> m >> k&&n&&m)
 	{
-		int cnt = 0;
-		while (n--)
+		memset(arr, 0, sizeof arr); memset(cnt, 0, sizeof cnt);
+		while (k--)
 		{
+			int x1, x2, y1, y2;
 			cin >> x1 >> y1 >> x2 >> y2;
-			for (int i = min(x1, x2); i <= max(x1, x2);i++)
-			for (int j = min(y1, y2); j <= max(y1, y2); j++)
-				arr[i][j] = true;
+			if (x1 > x2)
+				swap(x1, x2);
+			if (y1 > y2)
+				swap(y1, y2);
+			arr[x1][y1]++;
+			arr[x2 + 1][y1]--;
+			arr[x1][y2 + 1]--;
+			arr[x2 + 1][y2 + 1]++;
 		}
+		for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= m; j++)
+			arr[i][j] += arr[i][j - 1];
 
-		for (int i = 1; i <= w; i++)
-		for (int j = 1; j <= h; j++)
-		if (arr[i][j] == false)
-			cnt++;
+		for (int j = 1; j <= m; j++)
+		for (int i = 1; i <= n; i++)
+			arr[i][j] += arr[i-1][j];
 
-		cout << "There ";
-		if (cnt == 0)
-			cout << "is no empty spots." << finl;
-		else if (cnt == 1)
-			cout << "is one empty spot." << finl;
+		for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= m; j++)
+			cnt[i][j] += cnt[i][j - 1] + (arr[i][j] >= 1);
+
+		for (int j = 1; j <= m; j++)
+		for (int i = 1; i <= n; i++)
+			cnt[i][j] += cnt[i-1][j];
+		
+		if (n*m - cnt[n][m] <= 1)
+			cout << "There is " << (n*m - cnt[n][m] == 0 ? "no empty spots." : "one empty spot.") << finl;
 		else
-			cout <<"are "<< cnt<< " empty spots." << finl;
-		for (int i = 1; i <= w;i++)
-		for (int j = 1; j <= h; j++)
-			arr[i][j] = false;	
+			cout << "There are " << n*m - cnt[n][m] << " empty spots." << finl;
 	}
-}
+	
+} 
